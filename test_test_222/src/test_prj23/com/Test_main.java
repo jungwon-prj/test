9aa083f759a683a32aff3b66b9249bc1897626e6
jungwon-prj <ghddhfn1858@naver.com>
2025-01-20 19:39:56 +0900

    커밋 테스트

--- test_test_222/src/test_prj23/com/Test_main.java
+++ test_test_222/src/test_prj23/com/Test_main.java
@@ -3,8 +3,7 @@ package test_prj23.com;
 public class Test_main {
 
 	public static void main(String[] args) {
-		System.out.println("이것은 깃허브테스트여");
-		System.out.println("이것은 깃허브테스트여");
+		System.out.println("이것은 깃 테테ㅔ테테테테테");
 		System.out.println("가즈아");
 
 	}
